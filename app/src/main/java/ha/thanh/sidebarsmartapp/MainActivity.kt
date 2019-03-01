6022d17cbb1319670ba9caff935cde5c858fe409
Thanh.Ha <thanhahvan1@gmail.com>
2019-03-01 23:52:23 +0700

    ajfsf

--- app/src/main/java/ha/thanh/sidebarsmartapp/MainActivity.kt
+++ app/src/main/java/ha/thanh/sidebarsmartapp/MainActivity.kt
@@ -5,28 +5,29 @@ import android.os.Handler
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.WindowManager
 import com.facebook.shimmer.ShimmerFrameLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    var mShimmerViewContainer: ShimmerFrameLayout? = null
+//    var mShimmerViewContainer: ShimmerFrameLayout? = null
     var mContentView: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mShimmerViewContainer = findViewById(R.id.shimmer_view_container)
+//        mShimmerViewContainer = findViewById(R.id.shimmer_view_container)
         mContentView = findViewById(R.id.data_view)
 
 
-        mShimmerViewContainer!!.startShimmer()
+//        mShimmerViewContainer!!.startShimmer()
 
         Handler().postDelayed({
             onStopAni()
-            mShimmerViewContainer?.visibility = View.GONE
+//            mShimmerViewContainer?.visibility = View.GONE
             mContentView?.visibility = View.VISIBLE
         }, 5000)
 
@@ -41,9 +42,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onStopAni() {
-        // Stopping Shimmer Effect's animation after data is loaded to ListView
-        mShimmerViewContainer!!.stopShimmer()
-        mShimmerViewContainer!!.visibility = View.GONE
+//        // Stopping Shimmer Effect's animation after data is loaded to ListView
+//        mShimmerViewContainer!!.stopShimmer()
+//        mShimmerViewContainer!!.visibility = View.GONE
     }
 
 }
